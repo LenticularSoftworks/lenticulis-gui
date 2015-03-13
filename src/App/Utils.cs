9a09cd51c8f35bd882449a931c5702fd354c5675
MartinUbl <cmaranec@seznam.cz>
2015-03-14 00:19:38 +0100

    Added base Utils class to hold static methods containing often reused routines

---
+++ src/App/Utils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lenticulis_gui.src.App
+{
+    static class Utils
+    {
+        /// <summary>
+        /// Builds C-compliant string (zero terminated)
+        /// </summary>
+        /// <param name="input">Input string to be formatted</param>
+        /// <returns>C-compliant zero terminated char array</returns>
+        public static char[] getCString(String input)
+        {
+            return (input + '\0').ToCharArray();
+        }
+
+        /// <summary>
+        /// Converts color from ImageMagick format to ours
+        /// </summary>
+        /// <param name="number">input color-representing integer</param>
+        /// <returns>converted ARGB color integer</returns>
+        public static int convertColor(uint number)
+        {
+            // alpha channel is inverted
+            uint alpha = 255 - ((number & 0xFF000000) >> 24);
+            // remove old alpha and subtitute it with inverted one
+            int c = (int)((number & 0x00FFFFFF) | (alpha << 24));
+            return c;
+        }
+    }
+}
