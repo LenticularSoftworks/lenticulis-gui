a8dbcb4defbb672d0234ef31e6cd7e40a612566e
tkleisner <thomas.kl@seznam.cz>
2015-03-23 15:40:11 +0100

    GUI layout and file browser

---
+++ src/MainWindow/BrowserItem.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using System.Windows;
+
+namespace lenticulis_gui
+{
+    /// <summary>
+    /// File browser Item
+    /// </summary>
+    class BrowserItem
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Extension { get; set; }
+        public bool Dir { get; set; }
+        public string Ico { get; set; }
+
+        private const string PATH = "/lenticulis-gui;component/res/icon/";
+
+        public BrowserItem(string name, string path, string extension, bool dir)
+        {
+            this.Name = name;
+            this.Path = path;
+            this.Extension = extension.ToLower();
+            this.Dir = dir;
+            FindIcon();
+        }
+
+        /// <summary>
+        /// Finds icon by file extension
+        /// </summary>
+        private void FindIcon()
+        {
+            // If file is acceptable format
+            for (int i = 0; i < MainWindow.Extensions.Length; i++)
+            {
+                if (Extension.Equals(MainWindow.Extensions[i]))
+                {
+                    Ico = PATH + "Image.ico";
+
+                    return;
+                }
+            }
+
+            // Other extensions
+            switch (Extension)
+            {
+                case "drive": Ico = PATH + "Disc.ico"; break;
+                case "dir": Ico = PATH + "Folder.ico"; break;
+                case "parent": Ico = PATH + "Trackback.ico"; break;
+                default: Ico = PATH + "Unknown.ico"; break;
+            }
+        }
+
+        /// <summary>
+        /// To string
+        /// </summary>
+        /// <returns>Name of file</returns>
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
--- src/MainWindow/MainWindow.xaml.cs
+++ src/MainWindow/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
 
 namespace lenticulis_gui
 {
@@ -20,9 +22,97 @@ namespace lenticulis_gui
     /// </summary>
     public partial class MainWindow : Window
     {
+        //List of acceptable file extensions
+        public static string[] Extensions = { ".jpg", ".png", ".gif" };
+
         public MainWindow()
         {
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
             InitializeComponent();
+
+            GetDrives();
+        }
+
+        /// <summary>
+        /// Write list of drives into file browser.
+        /// </summary>
+        private void GetDrives()
+        {
+            List<BrowserItem> items = new List<BrowserItem>();
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            for (int i = 0; i < drives.Length; i++)
+            {
+                items.Add(new BrowserItem(drives[i].Name, drives[i].Name, "drive", true));
+            }
+
+            BrowserList.ItemsSource = items;
+            AddressBlock.Text = "Tento počítač";
+        }
+
+        /// <summary>
+        /// Load directory contents.
+        /// </summary>
+        /// <param name="path">Directory path</param>
+        private void ActualFolder(String path)
+        {
+            //List of files
+            List<BrowserItem> items = new List<BrowserItem>();
+
+            DirectoryInfo dir = new DirectoryInfo(path);
+            DirectoryInfo[] directories = dir.GetDirectories().Where(file => (file.Attributes & FileAttributes.Hidden) == 0).ToArray();
+            FileInfo[] files = dir.GetFiles();
+
+            //Add path to parent
+            if (dir.Parent != null)
+            {
+                items.Add(new BrowserItem("..", dir.Parent.FullName, "parent", true));
+            }
+            else if (dir != dir.Root)
+            {
+                items.Add(new BrowserItem("..", "root", "parent", true));
+            }
+
+            //Add directories
+            for (int i = 0; i < directories.Length; i++)
+            {
+                items.Add(new BrowserItem(directories[i].Name, directories[i].FullName, "dir", true));
+            }
+
+            //Add files
+            for (int i = 0; i < files.Length; i++)
+            {
+                items.Add(new BrowserItem(files[i].Name, files[i].Directory.ToString(), files[i].Extension, false));
+            }
+
+            AddressBlock.Text = dir.FullName;
+            BrowserList.ItemsSource = items;
+        }
+
+        /// <summary>
+        /// Open folder by selected item in browser
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Browser_DoubleClick(object sender, EventArgs e)
+        {
+            BrowserItem BItem = (BrowserItem)(BrowserList.SelectedItem);
+
+            if (BItem.Dir && BItem.Path != "root")
+            {
+                try
+                {
+                    ActualFolder(BItem.Path);
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.Forms.MessageBox.Show(ex.Message, "Nelze otevřít", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else if (BItem.Dir && BItem.Path == "root")
+            {
+                GetDrives();
+            }
         }
     }
 }
